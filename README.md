3256f8e8a9f9dfaece4a1caa30ad7a30b2dd738d
Fernando Carlos <111884680+Solarck@users.noreply.github.com>
2022-11-04 20:33:37 -0300

    up
    projeto completo

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # <div align="center">Relogio Digital</div>
  
 <div align="center"> 
- <img align="center" src="https://github.com/Solarck/Relogio-Digital/blob/main/relogio.png">
+ <img align="center" src="img/img-for-readme.png">
+
 </div>
---
+++ script.js
@@ -0,0 +1,42 @@
+const horas = document.getElementById('horas')
+const minutos = document.getElementById('minutos')
+const segundos = document.getElementById('segundos')
+
+const relogio = setInterval(function tempo() {
+
+    let hoje = new Date()
+    let hr = hoje.getHours()
+    let min = hoje.getMinutes()
+    let sec = hoje.getSeconds()
+
+    if (hr < 10) {
+        hr = '0' + hr
+    }
+
+    if (min < 10) {
+        min = '0' + min
+    }
+
+    if (sec < 10) {
+        sec = '0' + sec
+    }
+
+
+
+    horas.textContent = hr
+    minutos.textContent = min
+    segundos.textContent = sec
+
+})
+
+data = new Date()
+diasemana = data.getDay()
+dia = data.getDate()
+mes = data.getMonth()
+ano = data.getFullYear()
+
+meses = new Array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro',)
+
+dias = new Array('Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta-Feira', 'Sexta', 'Sabado')
+
+document.getElementById('data').innerHTML = dias[diasemana] + ', ' + dia + ' de  ' + meses[mes] + ' de  ' + ano
